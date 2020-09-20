b8cc87a2610dd17434940e6f6291c08989b5d0a4
Ryan Frazier <rfrazier716@gmail.com>
2020-09-20 12:53:28 -0400

    updated program counter to use asynchronous latch.

--- rtl/ProgramCounter.v
+++ rtl/ProgramCounter.v
@@ -1,11 +1,12 @@
 
 `default_nettype none //stops mispelled identifiers from being turned into wires
+
 parameter PC_RESET_ADDR = 16'h0000; //Value the program counter gets reset to on 
 
 module ProgramCounter(
     input wire sys_clock, // The system clock signal
     input wire reset, // Reset Signal
-    input wire phase_1, phase_2, // The clock phases
+    input wire phase_2_rising, //Signal asserts itself for one clock cycle on the rising edge of the phase 2 clock
     input wire pcl_pcl, adl_pcl, pch_pch,adh_pch, //signals that assert where to load the next PC address from, split for high and low
     input wire pcl_db, pcl_adl, pch_db,pch_adh, //signals that put the PC onto different IO buses
     input wire inc_pcl, //wire to assert that the next program counter address should the select_register + 1
@@ -19,41 +20,69 @@ module ProgramCounter(
     reg[15:0] program_counter_select; // program counter select register gets the next PC address to be loaded on phase 2
     initial program_counter = PC_RESET_ADDR; //initial value is the reset address
     initial program_counter_select = 0; //initial the PC select to zero
+    
+    //Set up synchronous rising edge outputs for pcl_pcl, adl_pcl, pch_pch, and adh_pch
+    wire pcl_pcl_r, adl_pcl_r, pch_pch_r, adh_pch_r;
+
+    /* verilator lint_off PINMISSING */
+    AsynchronousLatch pcl_pcl_l(
+        .sys_clock(sys_clock),
+        .reset(reset),
+        .async_signal(pcl_pcl),
+        .rising_edge(pcl_pcl_r)
+    );
+
+    AsynchronousLatch adl_pcl_l(
+        .sys_clock(sys_clock),
+        .reset(reset),
+        .async_signal(adl_pcl),
+        .rising_edge(adl_pcl_r)
+    );
+
+    AsynchronousLatch pch_pch_l(
+        .sys_clock(sys_clock),
+        .reset(reset),
+        .async_signal(pch_pch),
+        .rising_edge(pch_pch_r)
+    );
+
+    AsynchronousLatch adh_pch_l(
+        .sys_clock(sys_clock),
+        .reset(reset),
+        .async_signal(adh_pch),
+        .rising_edge(adh_pch_r)
+    );
+
+    /* verilator lint_on PINMISSING */
 
     //Synchronous Section
     always@(posedge sys_clock)
     begin
-        //on a positive clock edge can be in either phase 1 or phase 2 of a cpu cycle
         if(reset)
             program_counter <= PC_RESET_ADDR; //reset the program counter address
+
+        //Check if a rising edge occured on any of the program_counter_high latches
+        if(pch_pch_r)       program_counter_select[15:8]<=program_counter[15:8];
+        else if(adh_pch_r)  program_counter_select[15:8]<=address_h;
+        else                program_counter_select[15:8]<=program_counter_select[15:8];
         
-        //in phase 1 the new address is selected
-        if(phase_1) 
-        begin
-            //select the lower byte of the program counter
-            if(pcl_pcl) program_counter_select[7:0]<=program_counter[7:0];
-            if(pcl_adl) program_counter_select[7:0]<=address_l;
-            
-            //select the upper byte fo the program counter
-            if(pch_pch) program_counter_select[15:8]<=program_counter[15:8];
-            if(pch_adh) program_counter_select[15:8]<=address_h;
-        end
-        
+        //Check if a rising edge occured on any of the program_counter_low latches
+        if(pcl_pcl_r)       program_counter_select[7:0]<=program_counter[7:0];
+        else if(adl_pcl_r)  program_counter_select[7:0]<=address_l;
+        else                program_counter_select[7:0]<=program_counter_select[7:0];
+
         //in phase 2 we load the new PC address into memory
-        if(phase_2)
-        begin
-            program_counter<=program_counter_select+{15'b0,inc_pcl}; //Load the next program counter value
-        end
-        
-        
+        if(phase_2_rising)
+            program_counter<=program_counter_select+{15'b0,inc_pcl}; //Load the next program counter value     
     end
 
     //Assert the PC onto the databus
-    assign data_bus = pcl_db ? program_counter[7:0] : 8'hZZ; 
     assign data_bus = pch_db ? program_counter[15:8] : 8'hZZ;
-
+    assign data_bus = pcl_db ? program_counter[7:0] : 8'hZZ; 
+    
     //Assert the PC onto the address buses
-    assign address_l = pcl_adl ? program_counter[7:0] : 8'hZZ;
     assign address_h = pch_adh ? program_counter[15:8] : 8'hZZ;
+    assign address_l = pcl_adl ? program_counter[7:0] : 8'hZZ;
+    
 
 endmodule
--- verilator/verilator_pc.sh
+++ verilator/verilator_pc.sh
@@ -1,6 +1,6 @@
 module_name=ProgramCounter
 obj_dir="obj_dir/$module_name"
 mkdir -p $obj_dir
-verilator -Wall -cc "../rtl/$module_name.v" --Mdir $obj_dir
+verilator -Wall -cc "../rtl/$module_name.v" "../rtl/AsynchronousLatch.v" --Mdir $obj_dir
 cd $obj_dir
 make -f "V$module_name.mk"
