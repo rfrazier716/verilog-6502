8fcc80edc888cd13c185836f899dc75294762fc2
Ryan Frazier <rfrazier716@gmail.com>
2020-09-23 08:25:11 -0400

    Program Counter test WIP

--- test/src/testPC.cpp
+++ test/src/testPC.cpp
@@ -16,20 +16,76 @@
 // Catch Library
 #include "inc/catch.hpp"
 
-TEST_CASE("Rising Edge Functional","[aSyncLatch]"){
-    //When a rising edge occurs, on the next clock cycle it will the rising edge bit will be set
-    auto* tb = new SyncTB<MODTYPE>(50000000,false); // make a testbench of the module
-    tb->reset(); //reset the program counter
-    tb->dut->pch_pch=1;
-    tb->dut->pcl_pcl=1;
+template<class T>
+void latchDataLine(SyncTB<MODTYPE>* tb, T* dataLine){
+    *dataLine=1;
     tb->tick();
     tb->tick();
-    tb->dut->increment_pc=1;
+    *dataLine=0;
+}
+
+void cyclePhase2(SyncTB<MODTYPE>* tb){
     tb->dut->phase_2_rising=1;
     tb->tick();
-    printf("PC_ADDR=0x%04X\n",tb->dut->ProgramCounter__DOT__program_counter);
-    tb->tick();
-    printf("PC_ADDR=0x%04X\n",tb->dut->ProgramCounter__DOT__program_counter);
-    tb->tick();
-    printf("PC_ADDR=0x%04X\n",tb->dut->ProgramCounter__DOT__program_counter);
+    tb->dut->phase_2_rising=0;
 }
+
+//Cases to Test
+//Program counter reset
+TEST_CASE("Reset Functional","[PC]"){
+    auto* tb = new SyncTB<MODTYPE>(50000000,false); // make a new module test bench
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter==0x0000); //Assert that the PC initializes to zero
+    auto newPCValue=0xF030;
+    tb->dut->ProgramCounter__DOT__program_counter=newPCValue; // assign a new address to the PC
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter==newPCValue); //Make sure the address updated
+    tb->reset(); // Reset the device
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter==0x0000); //Assert that the program counter address reset on a reset    
+}
+
+//Incrementing the PC when in phase 2 of the clock
+TEST_CASE("PC Increments on Phase 2 rising edge","[PC]"){
+    auto* tb = new SyncTB<MODTYPE>(50000000,false); // make a new module test bench
+
+    //Load the current PC address into the next address
+    latchDataLine(tb, &tb->dut->pcl_pcl);
+    latchDataLine(tb, &tb->dut->pch_pch);
+
+    tb->dut->increment_pc=1; // Set the program counter to increment on the next phase 2;
+    tb->tick();
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter==0x0000); // require that the PC address hasn't updated
+    cyclePhase2(tb);
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter==0x0001); // on a phase 2 rising edge the address updates
+    cyclePhase2(tb);
+    //If we don't relatch the increment register nothing changes
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter==0x0001);
+
+    tb->dut->increment_pc=0;
+    cyclePhase2(tb);
+    //If we drop the increment flag back to zero without updating the address it goes back to zero
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter==0x0000);
+}
+
+TEST_CASE("Latching the Program Counter Select Register","[PC]"){
+    auto* tb = new SyncTB<MODTYPE>(50000000,false); // make a new module test bench
+    tb->dut->ProgramCounter__DOT__program_counter=0xDEAD;
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter_select==0x0000); //register initializes to zero
+
+    //latching PCS from PC
+    //TODO: Figure out if the latch is working right, plot with GTKWAVE
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter_select==0xDE00);
+    latchDataLine(tb,&tb->dut->pcl_pcl);
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter_select==0xDEAD);
+
+    //latching PCS from the Address bus
+    tb->reset(); //reset the system
+    tb->dut->address_l=0xEF;
+    tb->dut->address_h=0xBE;
+    latchDataLine(tb, &tb->dut->adh_pch);
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter_select==0xBE00);
+    latchDataLine(tb, &tb->dut->adl_pcl);
+    REQUIRE(tb->dut->ProgramCounter__DOT__program_counter_select==0xBEEF);
+}
+
+//Keeping the program counter constant on phase 2 of the clock
+//Loading the next PC Address from the existing PC Address
+//Loading the next PC Address from the address bus
